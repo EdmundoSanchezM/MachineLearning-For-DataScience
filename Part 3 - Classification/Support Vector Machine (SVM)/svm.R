95648c0079ef43b533850cf96e5dd93197029921
EdmundoSanchezM <edmundoelpro1@gmail.com>
2023-04-17 23:38:53 -0600

    Support Vector Machine
    SVM clasificador

--- Part 3 - Classification/Support Vector Machine (SVM)/svm.R
+++ Part 3 - Classification/Support Vector Machine (SVM)/svm.R
@@ -1,4 +1,4 @@
-# Plantilla de Clasificación
+# SVM
 
 #importando dataset
 
@@ -20,11 +20,14 @@ testing_set[,1:2] = scale(testing_set[,1:2])
 
 
 # Ajustar el clasificador con el conjunto de entrenamiento.
-# Crear el modelo de clasificacion
-
-
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = "C-classification",
+                 kernel = "linear")
+  
+  
 # Predicción de los resultados con el conjunto de testing
-
 y_pred = predict(classifier,newdata = testing_set[,-3])
 
 # Crear la matriz de confusión
@@ -40,7 +43,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Clasificación (Conjunto de Entrenamiento)',
+     main = 'SVM (Conjunto de Entrenamiento)',
      xlab = 'Edad', ylab = 'Sueldo Estimado',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -56,7 +59,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Clasificación (Conjunto de Testing)',
+     main = 'SVM (Conjunto de Testing)',
      xlab = 'Edad', ylab = 'Sueldo Estimado',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
