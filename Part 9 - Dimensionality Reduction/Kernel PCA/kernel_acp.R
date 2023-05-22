e8c4ea16d23c7fdf5a6a11f41af68c29c94913e7
EdmundoSanchezM <edmundoelpro1@gmail.com>
2023-05-21 21:48:03 -0600

    Kernel ACP and K-Fold cross Validation

---
+++ Part 9 - Dimensionality Reduction/Kernel PCA/kernel_acp.R
@@ -0,0 +1,81 @@
+# Kernel ACP
+
+#importando dataset
+
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[,3:5]
+#Dividir data set entre conjunto de entranamiento y testing
+library(caTools)
+set.seed(123) #Seleccionando semilla
+#sample.split(Datos a separar, porcentaje para entrenar)
+split = sample.split(dataset$Purchased,SplitRatio = 0.75)
+#Separando datos de entrenamiento y testing
+training_set = subset(dataset,split==TRUE)
+testing_set = subset(dataset,split==FALSE)
+
+#Escalado de valores
+training_set[,1:2] = scale(training_set[,1:2])#[Todas las filas, de 2 a 3]
+testing_set[,1:2] = scale(testing_set[,1:2])
+
+# Aplicar Kernel ACP
+#install.packages("kernlab")
+library(kernlab)
+kpca = kpca(~., data = training_set[,-3], kernel = "rbfdot", features = 2)
+training_set_pca = as.data.frame(predict(kpca,training_set))
+training_set_pca$Purchased = training_set[,3]
+
+testing_set_pca =  as.data.frame(predict(kpca,testing_set))
+testing_set_pca$Purchased = testing_set[,3]
+
+# Ajustar el modelo de regresión logística con el conjunto de entrenamiento.
+# Familia debe de ser binomial para regresion logistica
+classifier = glm(formula = Purchased ~ .,
+                 data = training_set_pca, 
+                 family = binomial)
+
+# Predicción de los resultados con el conjunto de testing
+#Probabilidades de compra o no compra
+prob_pred = predict(classifier, type = "response",
+                    newdata = testing_set_pca[,-3])
+#Si en la probabilidad es mayor a 0.5 sera 1 (compra), caso contrarion 0 (no compra)
+y_pred = ifelse(prob_pred> 0.5, 1, 0)
+
+# Crear la matriz de confusión
+cm = table(testing_set_pca[, 3], y_pred)
+
+# Visualización del conjunto de entrenamiento
+#install.packages("ElemStatLearn")
+library(ElemStatLearn)
+set = training_set_pca
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.06)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.06)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('V1', 'V2')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Clasificación (Conjunto de Entrenamiento)',
+     xlab = 'C1', ylab = 'C2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
+# Visualización del conjunto de testing
+set = testing_set_pca
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.06)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.06)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('V1', 'V2')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Clasificación (Conjunto de Testing)',
+     xlab = 'C1', ylab = 'C2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
